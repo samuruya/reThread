f5ea6fa7673d04fcf68831db98b9d37a644440c8
Samuruya <mcboyhd.de@gmail.com>
2023-12-24 17:32:55 +0100

    release

---
+++ bot.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+
+const { Client, IntentsBitField, ThreadChannel } = require('discord.js');
+const client = new Client({ intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+const token = process.env.BOT_TOKEN;
+const guildId = process.env.GUILD_ID;
+var active = true;
+
+const initClient = async () => {
+    client.on('ready', async () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+    });
+
+  await client.login(token);
+  return true;
+};
+
+
+
+// Method to fetch all channels in a guild
+const fetchAllChannels = async () => {
+    console.log("running faC...")
+    const guild = client.guilds.cache.get(guildId);
+    if (!guild) {
+        console.error(`Guild with ID ${guildId} not found`);
+        return;
+    }
+
+    const channels = guild.channels.cache;
+    channels.forEach(async (channel) => {
+        // Check if the channel is a thread
+        if (channel.isThread()) {
+            // Autojoin the thread
+            try {
+                await channel.join();
+            } catch (error) {
+            }
+
+            channel.setArchived(false)
+            if(channel.archived = true) {
+                console.log(channel.name)
+                channel.send({
+                    content: "refresh",
+                    flags: [ 4096 ]
+                  })
+                .then(msg => {
+                    setTimeout(() => msg.delete(), 10)
+                })
+            }
+        }
+    });
+    console.log("faC finished...")
+};
+
+async function main() {
+    const status = await initClient();
+    console.log(status);
+    await new Promise(resolve => setTimeout(resolve, 500));
+    var runCount = 0;
+    while(true) {
+        if(active) {
+            runCount = runCount + 1;
+            console.log(runCount);
+            await new Promise(resolve => setTimeout(resolve, 86400000));
+            await fetchAllChannels();
+        }
+    }
+}
+main();
+
+
+
+
+client.on("interactionCreate", (i) => {
+    if(!i.isChatInputCommand()) return;
+
+    if(i.commandName == "hey")
+        i.reply("hey");
+
+     if(i.commandName == "refresh") {
+        fetchAllChannels()
+     }
+
+
+        
+})
---
+++ command.js
@@ -0,0 +1,52 @@
+require("dotenv").config()
+const { REST, Routes } = require("discord.js");
+const token = process.env.BOT_TOKEN;
+const botId = process.env.BOT_ID;
+const guildId = process.env.GUILD_ID;
+
+const commands = [
+    {
+        name: "hey",
+        description: "Says Hey"
+    },
+
+    {
+        name: "refresh",
+        description: "sets all threads as active"
+    },
+    {
+        name: "on",
+        description: "auto active on"
+    },
+    {
+        name: "off",
+        description: "auto active off"
+    },
+];
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+
+    try {
+  
+      console.log("Registering slash commands...");
+  
+      await rest.put(
+  
+        Routes.applicationGuildCommands(botId, guildId),
+  
+        { body: commands }
+  
+      );
+  
+      console.log("Slash commands registered.");
+  
+    } catch (error) {
+  
+      console.log(`Error: ${error}`);
+  
+    }
+  
+  })();
+  
